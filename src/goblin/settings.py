fb0338e474c8e3776482fb2b5da8570a6376108c
Dennis Gilbert <dennis@imapsync-02.oit.pdx.edu>
2011-06-18 08:03:57 -0700

    Modifications to deploy into production env

--- src/goblin/settings.py
+++ src/goblin/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/dennis/workspace/goblin/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/vol/goblin/src/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/dennis/workspace/goblin/templates'
+    '/vol/goblin/templates'
 )
 
 INSTALLED_APPS = (
