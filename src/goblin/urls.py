9258986b7d64ef64447235f68660d3d5b78b7ddd
Dennis Gilbert <dennis@pdx.edu>
2011-06-18 11:57:33 -0700

    Cleanup of URLs

--- src/goblin/urls.py
+++ src/goblin/urls.py
@@ -14,14 +14,15 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^media(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/dennis/workspace/xi/media'}),
-	(r'^js(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/dennis/workspace/xi/media/js'}),
-	(r'^status/js(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/dennis/workspace/xi/media/js'}),
-	(r'^images(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/dennis/workspace/xi/media/images'}),
-	(r'^status/images(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/dennis/workspace/xi/media/images'}),
+    (r'^media(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/vol/goblin/doc'}),
+	(r'^js(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/vol/goblin/doc/js'}),
+	(r'^status/js(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/vol/goblin/doc/js'}),
+	(r'^images(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/vol/goblin/doc/images'}),
+	(r'^status/images(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/vol/goblin/doc/images'}),
 	(r'^copy_progress', 'goblin.ghoul.views.copy_progress'),
 	(r'^status', 'goblin.ghoul.views.status'),
 	(r'^confirm', 'goblin.ghoul.views.confirm'),
+	(r'^select', 'goblin.ghoul.views.confirm'),
 	(r'^', 'goblin.ghoul.views.select'),
 
 )
