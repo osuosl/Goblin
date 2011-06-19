95a8debe5cb5b94146f5fd7b584be9b66e772b2d
Dennis Gilbert <dennis@pdx.edu>
2011-06-19 11:59:47 -0700

    Added LDAP lookup to determine if user has already opt'd in. Also added form hidden fields to pass login-name between pages.

--- src/goblin/ghoul/views.py
+++ src/goblin/ghoul/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render_to_response
 #from django.http import HttpResponseRedirect
 from django.http import HttpResponse
+from django.template import RequestContext
 from django.core.cache import cache
 from tasks import copy_email_task
 #from celery.task import task
@@ -14,46 +15,55 @@ log = logging.getLogger('ghoul.views')
 
 def select(request):
 	#query = request.GET['q']
-	login = request.GET.get('login', '')
+	login = request.POST.get('login', '')
 	if not login:
-		login = 'weekse'
+		login = 'dennis'
 
-	log.info('views.select() called with user: ' + login)
+	log.info('views.select() called with login: ' + login)
 	
 	psu_sys = PSUSys()
 	large_emails = psu_sys.large_emails(login)
 			
 	return render_to_response('ghoul/select.html', 
 		{ 'login': login,
-		  #'long_folders': long_folders,
 		  'large_emails': large_emails,
-		  })
+		  },
+		context_instance=RequestContext(request),
+	)
 	
 def copy_progress(request):
 	psu_sys = PSUSys()
 	login = request.GET.get('login', '')
+	login = login.encode('latin-1')
+
+	log.info('views.copy_progress() called with login: ' + login)
+	
 	if not login:
 		login = 'weekse'
+
+	log.info('views.copy_progress() called with login: ' + login)
 
 	return HttpResponse(psu_sys.copy_progress(login))
 
 
 def status(request):
 	#query = request.GET['q']
-	login = request.GET.get('login', '')
-	if not login:
-		login = 'dennis'
+	login = request.POST.get('login', '')
+	login = login.encode('latin-1')
 
 	log.info('views.status() called with user: ' + login)
 	
-	key = 'email_copy_progress.' + login
-	if ( cache.get(key) == None ):
+	#key = 'email_copy_progress.' + login
+	#if ( cache.get(key) == None ):
+	#	log.info('views.status() cache.get(key): None')
+		
 		#copy_email_task.delay(login)
-		copy_email_task.apply_async(args=[login], queue='optin')
+	log.info('views.status() called celery task')
+	copy_email_task.apply_async(args=[login], queue='optin')
 		
 	return render_to_response('ghoul/status.html', 
-		{ 'login': login,
-				})
+		{ 'login': login, },
+		context_instance=RequestContext(request))
 	
 def confirm(request):
 	#query = request.GET['q']
@@ -70,6 +80,8 @@ def confirm(request):
 		{ 'login': login,
 		  'long_folders': long_folders,
 		  'large_emails': large_emails,
-		  })
+		  },
+		context_instance=RequestContext(request))
+
 		
 	
--- src/tasks.py
+++ src/tasks.py
@@ -5,15 +5,16 @@ from celery.log import logging
 
 @task
 def copy_email_task(login):
-	mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+	#mc = memcache.Client(['imapsync-mc.oit.pdx.edu:11211'], debug=0)
+	mc = memcache.Client(['localhost:11211'], debug=0)
 	timer = 90;
 	logging.info('test')
 	log = logging.getLogger()
-	log.info('tasks.copy_email_task(), login: ' + login )
 	#key = 'email_copy_progress.' + login
-	key = 'email_copy_progress.' + 'dennis'
+	key = 'email_copy_progress.' + login
 	while (timer <= 100):
 		mc.set(key, timer)
+		log.info('tasks.copy_email_task(), login: ' + login + ', timer: ' + str(timer))
 		timer = timer + 1
 		sleep(1)
 		
