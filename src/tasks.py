c01e1b58693a8c2a97b3a87209a196e68440e697
Dennis Gilbert <dennis@pdx.edu>
2011-06-20 13:26:42 -0700

    Update of LDAP server to contact for updates. Also unit-test updates.

--- src/tasks.py
+++ src/tasks.py
@@ -2,11 +2,15 @@ from celery.task import task
 import memcache
 from time import sleep
 from celery.log import logging
+from psuproperties import Property
+
 
 @task
 def copy_email_task(login):
 	#mc = memcache.Client(['imapsync-mc.oit.pdx.edu:11211'], debug=0)
-	mc = memcache.Client(['localhost:11211'], debug=0)
+	prop = Property( key_file = 'opt-in.key', properties_file = 'opt-in.properties')
+	memcache_url = prop.getProperty('memcache.url')
+	mc = memcache.Client([memcache_url], debug=0)
 	timer = 90;
 	logging.info('test')
 	log = logging.getLogger()
