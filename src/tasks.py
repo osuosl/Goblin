2072762d40bf223aec7044e9aa107dc496fd43e2
Dennis Gilbert <dennis@pdx.edu>
2011-06-21 12:57:26 -0700

    Route users to confimation page if they are already migrated.  Also setup final conversion task with each step for conversion.

--- src/tasks.py
+++ src/tasks.py
@@ -3,10 +3,11 @@ import memcache
 from time import sleep
 from celery.log import logging
 from psuproperties import Property
+from psusys import PSUSys
 
 
 @task
-def copy_email_task(login):
+def copy_email_task_null(login):
 	prop = Property( key_file = 'opt-in.key', properties_file = 'opt-in.properties')
 	log = logging.getLogger()
 	memcache_url = prop.getProperty('memcache.url')
@@ -21,5 +22,52 @@ def copy_email_task(login):
 		timer = timer + 1
 		sleep(1)
 		
+	return(True)
+
+def copy_email_task(login):
+	prop = Property( key_file = 'opt-in.key', properties_file = 'opt-in.properties')
+	log = logging.getLogger()
+	memcache_url = prop.getProperty('memcache.url')
+	mc = memcache.Client([memcache_url], debug=0)
+	psu_sys = PSUSys()
+
+	log.info("copy_email_task(): processing user: " + login)
+	key = 'email_copy_progress.' + login
+
+	# Check for LDAP mail forwarding already (double checking), if
+	# already opt'd-in, then immediately return and mark as complete.
+	
+	if (psu_sys.opt_in_already(login)):
+		mc.set(key, 100)
+		return(True)
+	else:
+		mc.set(key, 10)
+	
+	# Synchronize email to Google (and wait)
+	psu_sys.sync_email_null(login)
+	mc.set(key, 50)
+
+	# Final email sync
+	psu_sys.sync_email_null(login)
+	mc.set(key, 60)
+	
+	# Send conversion info email to users PSU account
+	psu_sys.send_conversion_email_null(login)
+	mc.set(key, 70)
+
+	# Switch routing of email to flow to Google
+	
+	psu_sys.route_to_google_null(login)
+	mc.set(key, 80)
+	
+	# Send conversion info email to users Google account
+	psu_sys.send_conversion_email_null(login)
+	mc.set(key, 90)
+
+	# Enable Google email for the user
+
+	psu_sys.enable_gmail_null(login)	
+	mc.set(key, 100)
+
 	return(True)
 	
