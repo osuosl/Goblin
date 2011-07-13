6da1c733e5201f1b8bbf7bceac4850f2d3255be6
Dennis Gilbert <dennis@pdx.edu>
2011-07-12 21:42:14 -0700

    Added sync_email IO buffering fix to delete2 task.

--- src/psusys.py
+++ src/psusys.py
@@ -429,11 +429,9 @@ mailRoutingAddress: %s@%s
 		google_pf = '/opt/google-imap/google-prod.pf'
 		
 		command = imapsync_cmd + " --pidfile /tmp/imapsync-full-" + login + ".pid --host1 " + imap_host + " --port1 993 --user1 " + login + " --authuser1 " + imap_login + " --passfile1 " + cyrus_pf + " --host2 imap.gmail.com --port2 993 --user2 " + login + "@" + 'pdx.edu' + " --passfile2 " + google_pf + " --ssl1 --ssl2 --maxsize 26214400 --delete2 --authmech1 PLAIN --authmech2 XOAUTH -sep1 '/' --exclude '^Shared Folders' "
-
-		syncprocess = subprocess.Popen(
-									shlex.split(command)
-									,stdout=subprocess.PIPE
-									,stderr=subprocess.PIPE )
+		log_file_name = '/tmp/imapsync-' + login + '-delete.log'
+		syncprocess = subprocess.Popen(	shlex.split(command), stdout = open(log_file_name, 'w') )
+									
 	# While the process is running, and we're under the time limit
 		while (syncprocess.poll() == None):
 			sleep(30)
