7ffc77cf3a80aad7ba4b71625b3074a0188cd441
Dennis Gilbert <dennis@pdx.edu>
2011-07-20 17:45:09 -0700

    Handle multi-valued fields for eduPersonAffiliation.

--- src/psusys.py
+++ src/psusys.py
@@ -70,10 +70,11 @@ class PSUSys:
 				#self.log.info('is_oamed() user: ' + login + ' has a ' + attr + ' of ' + str(result[attr]))
 				print('is_oamed() user: ' + login + ' has a ' + attr + ' of ' + str(result[attr]))
 
-				if str(result[attr]) in ['SPONSORED', 'SERVICE']:
-					# self.log.info('is_oamed() user: ' + login + ' is not OAMed' )
-					print('is_oamed() user: ' + login + ' is not OAMed' )
-					return False
+				for affiliation in result[attr]:
+					if affiliation in ['SPONSORED', 'SERVICE']:
+						# self.log.info('is_oamed() user: ' + login + ' is not OAMed' )
+						print('is_oamed() user: ' + login + ' is not OAMed' )
+						return False
 		return True
 
 	def get_ldap_attr(self, login, attr):
