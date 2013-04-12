ce837eb148a89bca448bcd066e3d74c71736df60
Justin Dugger <jldugger@osuosl.org>
2013-04-12 13:54:43 -0700

    move this up a level to deploy out of box

--- src/goblin/settings.py
+++ src/goblin/settings.py
@@ -150,7 +150,7 @@ LOGGING = {
 			'level': 'DEBUG',
 			'class': 'logging.handlers.RotatingFileHandler',
 			'formatter': 'dateTime',
-			'filename': '/var/log/goblin/goblin.log',
+			'filename': '/var/log/goblin.log',
 			'maxBytes':	4000000,
 			'backupCount': 5,
 		},
