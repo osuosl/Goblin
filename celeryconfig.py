29352f786b1ff5408f526e52b67c515f7d398a6e
Rudy Grigar <rudy@grigar.net>
2013-05-28 11:37:04 -0700

    testing the worker stuff

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,12 +1,12 @@
 #BROKER_HOST = 'sync.oit.pdx.edu'
-BROKER_HOST = 'localhost'
+BROKER_HOST = 'test-migbe.onid.oregonstate.edu'
 BROKER_PORT = 5672
 BROKER_USER = 'optin'
 BROKER_PASSWORD = 'CbXVtHJHFmwgE'
 BROKER_VHOST = 'optin'
 CELERY_RESULT_BACKEND = "amqp"
 CELERY_IMPORTS = ("tasks", )
-CELERYD_LOG_FILE = '/vol/goblin/var/celery.log'
+CELERYD_LOG_FILE = '/var/www/goblin/shared/celery.log'
 CELERYD_LOG_LEVEL = 'INFO'
 CELERY_ROUTES = {"tasks.copy_email_task": {"queue": "optin"}}
 
