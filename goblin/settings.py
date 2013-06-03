244b9a1ad696608335510f45e3e5eec0a1d922db
Justin Dugger <jldugger@osuosl.org>
2013-06-03 15:24:17 -0700

    more capistrano style wierdness

--- goblin/settings.py
+++ goblin/settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/var/www/goblin/templates'
+    '/var/www/goblin/current/goblin/templates'
 )
 
 INSTALLED_APPS = (
