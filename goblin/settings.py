d412ea5b7296c3e8b7d7c0d495b3c4600aa96c6d
Rudy Grigar <rudy@grigar.net>
2013-06-04 10:10:57 -0700

    fix the static file paths

--- goblin/settings.py
+++ goblin/settings.py
@@ -1,7 +1,19 @@
 # Django settings for goblin project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+# Repository directory.
+ROOT = os.path.dirname(os.path.dirname(__file__))
+
+# Django project directory.
+PROJECT_ROOT = os.path.dirname(__file__)
+
+#print PROJECT_ROOT
+
+# path bases things off of ROOT
+path = lambda *a: os.path.abspath(os.path.join(ROOT, *a))
 
 ADMINS = (
     ('Admin', 'root@localhost'),
@@ -56,7 +68,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = path('static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,6 +84,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    path('../media'),
 )
 
 # List of finder classes that know how to find static files in
