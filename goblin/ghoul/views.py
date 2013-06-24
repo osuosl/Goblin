b48a4211685d15998e069c42f7cde691821078d5
Justin Noah <jnoah@osuosl.org>
2013-06-24 15:29:20 -0700

    views: some selected logic cleanup
    refs #13683

--- goblin/ghoul/views.py
+++ goblin/ghoul/views.py
@@ -33,32 +33,29 @@ def select(request):
     psu_sys = PSUSys()
     psu_sys.set_user(login, request.META)
 
-    # Go to informational page for folks who are not yet allowed-in
     if not psu_sys.is_allowed(login):
+        # Go to informational page for folks who are not yet allowed-in
         return render_to_response('ghoul/closed.html', {'login': login},
                                   context_instance=RequestContext(request),)
-
-    # Go to the confirmation page if the user has already opt'd-in
-    if psu_sys.opt_in_already(login):
+    elif psu_sys.opt_in_already(login):
+        # Go to the confirmation page if the user has already opt'd-in
         return render_to_response('ghoul/confirm.html', {'login': login},
+                                  context_instance=RequestContext(request),)
+    elif psu_sys.is_web_suspended(login):
+        # Go to suspended page if site is not available
+        return render_to_response('ghoul/suspended.html', {'login': login},
                                   context_instance=RequestContext(request),)
 
     if psu_sys.is_processing(login):
         return render_to_response('ghoul/status.html', {'login': login})
 
-    # Go to suspended page if site is not available
-    if psu_sys.is_web_suspended(login):
-        return render_to_response('ghoul/suspended.html', {'login': login},
-                                  context_instance=RequestContext(request),)
     try:
         large_emails = psu_sys.large_emails(login)
     except:
         return render_to_response('ghoul/folderbomb.html', {'login': login},
                                   context_instance=RequestContext(request),)
 
-    log.error("psu_sys.presync_enabled(login): " +
-              psu_sys.presync_enabled(login))
-
+    # If nothing special, begin the wizard
     if psu_sys.presync_enabled(login):
         template = 'ghoul/form_wizard/step1yes.html'
     else:
