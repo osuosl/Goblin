9f0c4f96dde69fa5331a8b7618d754b37234de2a
Justin Noah <jnoah@osuosl.org>
2013-07-03 16:11:33 -0700

    views: fix typo

--- goblin/ghoul/views.py
+++ goblin/ghoul/views.py
@@ -55,7 +55,7 @@ class MigrationWizard(SessionWizardView):
         reiteration of all the pages the user just went through.
         """
 
-        return renter_to_respose('ghoul/form_wizard/step5done.html', {
+        return renter_to_response('ghoul/form_wizard/step5done.html', {
             'form_data': [form.cleaned_data for form in form_list],
         })
 
