2bd6a2706b66980244baf3cb9290f76c11b4f610
Justin Noah <jnoah@osuosl.org>
2013-07-05 11:54:07 -0700

    urls: removed commented out url lines

--- goblin/urls.py
+++ goblin/urls.py
@@ -12,10 +12,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    #(r'^media(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/var/www/goblin/doc'}),
-    #(r'^js(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/var/www/goblin/doc/js'}),
     (r'^status/js(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/var/www/goblin/doc/js'}),
-    #(r'^images(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/var/www/goblin/doc/images'}),
     (r'^status/images(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/var/www/goblin/doc/images'}),
     (r'^copy_progress', 'goblin.ghoul.views.copy_progress'),
     (r'^status', 'goblin.ghoul.views.status'),
