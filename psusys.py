d0226756ac1afc50e5ddf2928cf7459baa4df249
Justin Noah <jnoah@osuosl.org>
2013-07-16 20:04:37 -0700

    psusys: yay for celery, need to hardcode paths
    refs #13905

--- psusys.py
+++ psusys.py
@@ -17,8 +17,6 @@ from gdata.service import BadAuthentication
 from gdata.service import CaptchaRequired
 from gdata.apps.service import AppsForYourDomainException
 
-from django.conf import settings
-import celeryconfig
 
 class PSUSys:
     def __init__(self):
@@ -906,7 +904,7 @@ mailRoutingAddress: %s@%s
 
         # Send conversion info email to users Google account
         log.info("copy_email_task(): conversion in progress email: " + login)
-        psu_sys.send_conversion_email_in_progress(login, celeryconfig.ROOT)
+        psu_sys.send_conversion_email_in_progress(login, '/var/www/goblin/current/')
 
         # Enable Google email for the user
         # This is the last item that the user should wait for.
@@ -931,9 +929,9 @@ mailRoutingAddress: %s@%s
         log.info("copy_email_task(): Routing email to Google: " + login)
         #psu_sys.route_to_google(login)
         # Perl script to run
-        cmd = os.join(celeryconfig.ROOT, 'set-cyrus-fwd.pl')
+        cmd = '/var/www/goblin/current/bin/set-cyrus-fwd.pl'
         # Imap config file
-        config = os.join(celeryconfig.ROOT, 'etc', 'imap_fwd.cfg')
+        config = '/var/www/goblin/current/etc/imap_fwd.cfg'
         # Subprocess
         results = subprocess.Popen(['perl', cmd, config, login])
         # Communicate to get (stdout, stderr)
