fc034836dbbba95f97398e104e3ce010f8adc905
Justin Dugger <jldugger@osuosl.org>
2013-07-19 11:30:34 -0700

    CP#13737: don't log to /tmp

--- psusys.py
+++ psusys.py
@@ -754,9 +754,9 @@ mailRoutingAddress: %s@%s
         self.log.info(command)
 
         if extra_opts == '':
-            log_file_name = '/tmp/imapsync-' + login + '.log'
+            log_file_name = '/var/log/imapsync/imapsync-' + login + '.log'
         else:
-            log_file_name = '/tmp/imapsync-' + login + '-delete.log'
+            log_file_name = '/var/log/imapsync/imapsync-' + login + '-delete.log'
         syncprocess = subprocess.Popen(shlex.split(command),
                                        stdout=open(log_file_name, 'w'))
     # While the process is running, and we're under the time limit
@@ -794,7 +794,7 @@ mailRoutingAddress: %s@%s
         google_pf = '/opt/google-imap/google-prod.pf'
 
         command = imapsync_cmd + " --pidfile /tmp/imapsync-full-" + login + ".pid --host1 " + imap_host + " --port1 993 --user1 " + login + " --authuser1 " + imap_login + " --passfile1 " + cyrus_pf + " --host2 imap.gmail.com --port2 993 --user2 " + login + "@" + 'pdx.edu' + " --passfile2 " + google_pf + " --ssl1 --ssl2 --maxsize 26214400 --delete2 --delete2folders --authmech1 PLAIN --authmech2 XOAUTH -sep1 '/' --exclude '^Shared Folders' "
-        log_file_name = '/tmp/imapsync-' + login + '-delete.log'
+        log_file_name = '/var/log/imapsync/imapsync-' + login + '-delete.log'
         syncprocess = subprocess.Popen(shlex.split(command),
                                        stdout=open(log_file_name, 'w'))
 
