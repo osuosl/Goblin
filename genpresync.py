5966e4abd4baf91036e8fe86e59ae9ca685b267e
Justin Noah <jnoah@osuosl.org>
2013-11-15 18:42:50 -0800

    genpresync: options added -
    -l / --ldap (requires -p)
    -u / --userlist <file path>
    -s / --stdin (reads from stdin)
    
    -p / --password (required by -l)
    
    No options reads from stdin

--- genpresync.py
+++ genpresync.py
@@ -14,28 +14,20 @@ import argparse
 
 logging.getLogger('pika').setLevel(logging.WARN)
 
-class PreSync():
-    def __init__(self):
+class PreSync(object):
+    def __init__(self, ulist, ldap=False, password=None):
         self.fac_to_presync = []
         self.prop = Property(key_file = 'opt-in.key', properties_file = 'opt-in.properties')
         self.deny = self.prop['deny.users']
-        try:
-            opts, args = getopt.getopt(sys.argv[1:], 'p:')
-        except getopt.GetoptError as err:
-            print(err)
-            sys.exit()
-
-        for o,a in opts:
-            if o in ("-p", "--password"):
-                self.password = a
-            else:
-                print 'Missing password!'
-                sys.exit()
+        if ldap:
+            self.ulist = self.gen_ldap_list()
+        self.ulist = ulist
+        self.password = password
 
     def gen_presync_test(self):
         self.fac_to_presync = ['pittsh', ]
 
-    def gen_presync(self):
+    def gen_ldap_list(self):
         tmp_file = '/tmp/presync'
 
         command = 'ldapsearch -w ' + self.password +' -x -LLL -h ldap.onid.orst.edu -D uid=onid_googlesync,ou=specials,o=orst.edu -b ou=people,o=orst.edu "(&(!(googleMailEnabled=1))(googlePreSync=1))" uid osuUID sn givenName osuPrimaryAffiliation'
@@ -43,7 +35,10 @@ class PreSync():
         while (syncprocess.poll() == None):
             sleep(30)
 
-        fh = open(tmp_file)
+        return tmp_file
+
+    def gen_presync(self):
+        fh = open(self.ulist)
         lines = fh.readlines()
         haveRead = False
         firstName = lastName = loginName = pidm = id = ""
@@ -57,7 +52,7 @@ class PreSync():
                 try:
                     (a, v) = re.split(":", line)
                 except:
-                    break
+                   break
                 if (a == "uid"):
                     loginName = v.lstrip()
                     haveRead = True
@@ -82,6 +77,7 @@ class PreSync():
                 role = ''
                 haveRead = False
                 invalid = False
+
         random.shuffle(self.fac_to_presync)
 
     def get_presync_list(self):
@@ -105,12 +101,25 @@ class PreSync():
             presync_email_test_task.apply_async(args=[login], queue='optinpresync')
 
 if __name__ == '__main__':
+    # Arguments for script use
     parser = argparse.ArgumentParser(description="Presync tool")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-u", "--userlist", type=str, help="Presync users from a list")
-    group.add_argument("-l", "--ldap", type=str, help="Presync users from ldap")
-    group.add_argument("-s", "--stdin", action="store_true")
+    group.add_argument("-l", "--ldap", dest='ldap', action='store_true', help="Presync users from ldap")
+    group.add_argument("-s", "--stdin", action="store_true", help="Presync users from standard input")
+    parser.add_argument("-p", "--password", type=str, help="LDAP Password")
     args = parser.parse_args()
-    presync = PreSync()
+
+    # Handle the arguments
+    if args.ldap and not args.password:
+        print "LDAP requires option -p for a password\n"
+        parser.print_help()
+        sys.exit()
+    ulist = None
+    if not args.ldap and not args.stdin and not args.userlist:
+        ulist = sys.stdin
+
+    # Properly configure PreSync()
+    presync = PreSync(ulist=ulist, ldap=args.ldap, password=args.password)
     presync.gen_presync()
     presync.submit_task()
