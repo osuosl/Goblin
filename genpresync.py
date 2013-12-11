2ec298c08ef246300b64e72ef4107aaf667eeca1
Justin Noah <jnoah@osuosl.org>
2013-12-10 16:45:34 -0800

    genpresync: changed ldap search to only return uid

--- genpresync.py
+++ genpresync.py
@@ -30,7 +30,7 @@ class PreSync(object):
     def gen_ldap_list(self):
         tmp_file = '/tmp/presync'
 
-        command = 'ldapsearch -w ' + self.password +' -x -LLL -h ldap.onid.orst.edu -D uid=onid_googlesync,ou=specials,o=orst.edu -b ou=people,o=orst.edu "(&(!(googleMailEnabled=1))(googlePreSync=1))" uid osuUID sn givenName osuPrimaryAffiliation'
+        command = 'ldapsearch -w ' + self.password +' -x -LLL -h ldap.onid.orst.edu -D uid=onid_googlesync,ou=specials,o=orst.edu -b ou=people,o=orst.edu "(&(!(googleMailEnabled=1))(googlePreSync=1))" uid'
         syncprocess = subprocess.Popen(shlex.split(command), stdout=open(tmp_file, 'w'))
         while (syncprocess.poll() == None):
             sleep(30)
